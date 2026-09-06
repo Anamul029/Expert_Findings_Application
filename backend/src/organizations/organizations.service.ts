1f69d38a538fcf8c77c9e92cfbba913991b7c21a
jobaer6768 <jobaer9898@gmail.com>
2026-09-06 21:13:04 +0600

    Worked with api versioning and created /organizations Get method.

--- backend/src/organizations/organizations.service.ts
+++ backend/src/organizations/organizations.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateOrganizationDto } from './dto/create-organization.dto';
-import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Organization } from './entities/organization.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrganizationsService {
-  create(createOrganizationDto: CreateOrganizationDto) {
-    return 'This action adds a new organization';
-  }
+  constructor(
+    @InjectRepository(Organization)
+    private organizationRepository: Repository<Organization>,
+  ) {}
 
-  findAll() {
-    return `This action returns all organizations`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} organization`;
-  }
-
-  update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
-    return `This action updates a #${id} organization`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} organization`;
+  async findAll(): Promise<Organization[]> {
+    return this.organizationRepository.find();
   }
 }
