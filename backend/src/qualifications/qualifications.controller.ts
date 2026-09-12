09d298134fd4b4215994f5d5ecb18a1e6397efc5
jobaer6768 <jobaer9898@gmail.com>
2026-09-12 22:51:21 +0600

    completed working on /qualifications Get method

--- backend/src/qualifications/qualifications.controller.ts
+++ backend/src/qualifications/qualifications.controller.ts
@@ -1,34 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QualificationsService } from './qualifications.service';
-import { CreateQualificationDto } from './dto/create-qualification.dto';
-import { UpdateQualificationDto } from './dto/update-qualification.dto';
 
-@Controller('qualifications')
+@Controller({ path: 'qualifications', version: '1' })
 export class QualificationsController {
   constructor(private readonly qualificationsService: QualificationsService) {}
-
-  @Post()
-  create(@Body() createQualificationDto: CreateQualificationDto) {
-    return this.qualificationsService.create(createQualificationDto);
-  }
 
   @Get()
   findAll() {
     return this.qualificationsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.qualificationsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateQualificationDto: UpdateQualificationDto) {
-    return this.qualificationsService.update(+id, updateQualificationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.qualificationsService.remove(+id);
   }
 }
